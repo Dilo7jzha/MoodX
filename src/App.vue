2b6dc2cfab930e3ce9e936982c882eabc640aa05
Dilo7jzha <whateverittakes23@outlook.com>
2024-08-19 12:24:59 +1000

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>MoodX</title>
+        <link href="/styles.css" rel="stylesheet" />
   </header>
 
   <main>
-    <TheWelcome />
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <h1 id="title">MoodX</h1>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#services">Get Help</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Get involved</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">About us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
   </main>
 </template>
 
