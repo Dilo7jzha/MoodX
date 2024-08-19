95524015c80509186c57e30feb3615366bc587f1
Dilo7jzha <whateverittakes23@outlook.com>
2024-08-19 12:36:33 +1000

    use nav bar as Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,17 @@
 <script setup>
+import Header from './components/Header.vue';
+
 //import HelloWorld from './components/HelloWorld.vue'
 //import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>MoodX</title>
-        <link href="/styles.css" rel="stylesheet" />
+    <Header />
   </header>
 
   <main>
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-            <div class="container">
-                <h1 id="title">MoodX</h1>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars ms-1"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#services">Get Help</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#portfolio">Get involved</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#about">About us</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    
   </main>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- Using Bootstrap's Header template (starter code) -->
+    <!-- https://getbootstrap.com/docs/5.0/examples/headers/ -->
+    <div class="container">
+      <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item">
+            <a href="#" class="nav-link active" aria-current="page">MoodX</a>
+          </li>
+          <li class="nav-item"><a href="#" class="nav-link">Get help</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Get involved</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">About us</a></li>
+        </ul>
+      </header>
+    </div>
+  </template>
+  
+  <style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+  </style>
