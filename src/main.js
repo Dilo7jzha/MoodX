b79f12ac9041c94b6ae0a3c858481e35fbbfa92a
Dilo7jzha <whateverittakes23@outlook.com>
2024-09-25 18:48:32 +1000

    Firebase connfig

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 // import './assets/main.css'
 import './assets/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4tYRA-TakPKsod4_iDjBuhBo6b1JplQM",
+  authDomain: "moodx-a3040.firebaseapp.com",
+  projectId: "moodx-a3040",
+  storageBucket: "moodx-a3040.appspot.com",
+  messagingSenderId: "503884886491",
+  appId: "1:503884886491:web:66c30f5a0b1cf1f31d77fe"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 import { createApp } from 'vue'
 import App from './App.vue'
