974543f672d5f48a4ff099d1674a80f5aff5fede
Dilo7jzha <whateverittakes23@outlook.com>
2024-10-13 14:41:02 +1100

    remove unused pages

--- src/views/BookConsultationView.vue
+++ src/views/BookConsultationView.vue
@@ -2,7 +2,7 @@
     <div class="row mt-5">
         <div class="col-md-8 offset-md-2">
             <h1 class="text-center">Book Consultation</h1>
-            <p>Book Consultation page</p>
+            <p>Book Consultation page (yet to be implemented)</p>
         </div>
     </div>
 </template>
--- src/views/GetInvolvedView.vue
+++ src/views/GetInvolvedView.vue
@@ -2,7 +2,7 @@
     <div class="row mt-5">
         <div class="col-md-8 offset-md-2">
             <h1 class="text-center">Get Involved</h1>
-            <p>Get Involved page</p>
+            <p>Get Involved page (yet to be implemented)</p>
         </div>
     </div>
 </template>
